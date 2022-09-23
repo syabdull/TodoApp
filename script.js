438681a8fdf671e3d9cf4305ce76d54ef9db6607
syabdull <syab5050@gmail.com>
2022-09-23 09:06:18 +0530

    removal added

--- script.js
+++ script.js
@@ -7,12 +7,26 @@
                 console.log(value);
                 inputTag.value=" ";
                 let liTag=document.createElement("li");
-                liTag.innerText=value;
+                liTag.innerHTML=`<div>${value}</div>
+                                   <div class="delete"><i class="fa fa-trash"></i></div>`
                 ulTag.appendChild(liTag);
+                handleRemoval(liTag);
                 
                 
 
             }
 
       })
+
+
+
+       function handleRemoval(liTag){
+                      let deleteIcon  =liTag.querySelector(".delete");
+
+               deleteIcon.addEventListener("click",function(){
+                        liTag.remove();
+
+               })
+
+       }
       
